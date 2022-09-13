c80859fb85ff26e669ccfebca4a95aeaed716643
Dario Batista <80070216+BearProgrammmer@users.noreply.github.com>
2022-09-13 14:54:24 -0300

    Add files via upload

---
+++ Admin-Dashboard/Admin Dashboard/orders.js
@@ -0,0 +1,54 @@
+const Orders = [{
+    productName: 'Dollynho',
+    productNumber: '32165',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+},
+{
+    productName: 'Mouse',
+    productNumber: '65498',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+},
+{
+    productName: 'EnergyDrink',
+    productNumber: '10000',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined',
+},
+{
+    productName: 'Boohowaer',
+    productNumber: '44',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined',
+},
+{
+    productName: 'Keyboard',
+    productNumber: '32421',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+},
+{
+    productName: 'Death Star',
+    productNumber: '63126',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+},
+{
+    productName: 'Chocolate Bar',
+    productNumber: '43521',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+},
+{
+    productName: 'Notepad',
+    productNumber: '54875',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+},
+{
+    productName: 'Table',
+    productNumber: '67234',
+    paymentStatus: 'Paid',
+    shipping: 'Pending',
+}]
